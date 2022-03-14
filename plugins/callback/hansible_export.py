f78fddfe41dd5d45d4ddb7585c435ba71dee2311
Jonas Gunz <himself@jonasgunz.de>
2022-03-14 09:19:59 +0100

    LICENSE

--- plugins/callback/hansible_export.py
+++ plugins/callback/hansible_export.py
@@ -1,3 +1,13 @@
+#
+# hansible_export.py
+#
+#  Copyright (C) 2022 Jonas Gunz, Konstantin Grabmann, Paul Trojahn
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+
 from ansible.plugins.callback import CallbackBase
 
 import json
