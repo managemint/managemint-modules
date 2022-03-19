ad73358c0fa62756aaed0ec9f95a8e045db3123b
Jonas Gunz <himself@jonasgunz.de>
2022-03-19 18:12:23 +0100

    test

--- plugins/callback/hansible_export.py
+++ plugins/callback/hansible_export.py
@@ -116,6 +116,9 @@ class CallbackModule(CallbackBase):
         self._on_runner(result)
 
     def v2_runner_on_failed(self, result, ignore_errors=False):
+        if ignore_errors is None:
+            ignore_errors = False
+
         self._on_runner(result, ignore_errors = ignore_errors)
 
     def v2_runner_on_skipped(self, result):
